341fc353e1f29b6ff0b99c8db173ca0b2c11db17
99895bmw <99895bmw@gmail.com>
2026-08-30 14:31:19 +0530

    node practice

---
+++ 03-topic-wise-practice/03-node-and-express/fsModuleInNode/fs-module.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+// async read file
+// fs.readFile('path', optional encoding, call back function)
+fs.readFile('test.txt', 'utf-8', (err, data) => {
+    if (err) throw err
+    
+    console.log(data)
+})
+
+const content = 'hilakathamafiliya'
+fs.writeFile('newfile.txt', content, err => {
+  if (err) throw err
+  console.log('saved')
+})
+
+
+
+
+fs.appendFile('newfile.txt', '\ndhurandhar.', err => {
+  if (err) throw err;
+  console.log('appended');
+});
+
+fs.readFile('newfile.txt', 'utf-8', (err, data)=>{
+    if (err) throw err
+    console.log('after appending \n', data)
+})
+
